7d5fe420e6ababdd409d046724494f3a4fc66951
Jason55688 <jason0911423885@gmail.com>
2026-07-09 22:22:56 +0800

    Add 0707 score menu homework

---
+++ 0707/WhileInputDemo.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class WhileInputDemo {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int number = -1;
+
+        while (number != 0) {
+            System.out.print("Enter a number (0 to quit): ");
+            number = scanner.nextInt();
+
+            if (number != 0) {
+                System.out.println("You entered: " + number);
+            }
+        }
+
+        System.out.println("Program End");
+        scanner.close();
+    }
+}
